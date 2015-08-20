87ccd7422dd14d30e5768c46f1333315472e4981
SLTCP <lasslily2@gmail.com>
2015-08-20 18:18:31 +0800

    Create sltcp.c

---
+++ sltcp.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+
+
+
+FILE *fp;
+
+char returnData[64];
+
+fp = popen("/sbin/ifconfig eth0", "r");
+
+while (fgets(returnData, 64, fp) != NULL)
+
+{
+
+printf("%s" , returnData);
+
+}
+
+pclose(fp);
+
+}
